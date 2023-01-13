2f94086bc38783012fafd2529094eae5587f3f97
Andrey <teryaev.2004@mail.ru>
2023-01-13 23:59:51 +0300

    start-header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,19 @@
+import React, { FC } from 'react';
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 40px 0;
+`;
+
+const Header: FC = () => {
+  return (
+    <Wrapper>
+      
+    </Wrapper>
+  );
+};
+
+export default Header;
---
+++ src/components/Menu/Menu.tsx
@@ -0,0 +1,18 @@
+import React, { FC } from 'react';
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 26px;
+`;
+
+const Menu: FC = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default Menu;
