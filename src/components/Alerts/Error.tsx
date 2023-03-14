7ab65d1e437fdb11ddf072e854511bb13ef6f976
Andrey <teryaev.2004@mail.ru>
2023-03-15 01:02:02 +0300

    fix-alerts

--- src/components/Alerts/Error.tsx
+++ src/components/Alerts/Error.tsx
@@ -7,9 +7,9 @@ const Error: FC = () => {
 
   return (
     <div
-      className={`fixed top-[10px] right-[${
-        storeList.isError.isError ? "0" : "-100%"
-      }] rounded-l-[5px] bg-red-500 text-white ease-linear duration-500 flex justify-center items-center px-3 py-2 min-h-[40px]`}
+      className={`fixed top-[10px] ${
+        storeList.isError.isError ? "right-0" : "-right-[100%]"
+      } rounded-l-[5px] bg-red-500 text-white ease-linear duration-500 flex justify-center items-center px-3 py-2 min-h-[40px]`}
     >
       {storeList.isError.text}
     </div>
--- src/components/Alerts/Success.tsx
+++ src/components/Alerts/Success.tsx
@@ -7,9 +7,9 @@ const Success: FC = () => {
 
   return (
     <div
-      className={`fixed top-[10px] right-[${
-        storeList.isSuccess.isSuccess ? "0" : "-100%"
-      }] rounded-l-[5px] bg-green-500 text-white ease-linear duration-500 flex justify-center items-center px-3 py-2 min-h-[40px]`}
+      className={`fixed top-[10px] ${
+        storeList.isError.isError ? "right-0" : "-right-[100%]"
+      } rounded-l-[5px] bg-green-500 text-white ease-linear duration-500 flex justify-center items-center px-3 py-2 min-h-[40px]`}
     >
       {storeList.isSuccess.text}
     </div>
