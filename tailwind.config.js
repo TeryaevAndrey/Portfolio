5518f4b3aa51363bae7f472b33a8267b57857d4b
Andrey <teryaev.2004@mail.ru>
2023-07-17 00:15:12 +0300

    change bg-s

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,9 +24,9 @@ module.exports = {
         "skills-gradient":
           "linear-gradient(180deg, rgba(53, 110, 152, 0.25) 0%, rgba(11, 46, 226, 0.3) 100%)",
         "callback-gradient":
-          "linear-gradient(139.05deg, rgba(11, 46, 226, 0.3) 28.92%, rgba(11, 46, 226, 0.3) 28.93%, rgba(93, 108, 187, 0.41) 80.1%, rgba(0, 19, 115, 0.3) 100%)",
+          "linear-gradient(180deg, rgba(11, 46, 226, 0.3) 100%, rgba(53, 110, 152, 0.25) 0%)",
         "footer-gradient":
-          "linear-gradient(83.26deg, rgba(45, 90, 123, 0.35) 0%, rgba(11, 46, 226, 0.3) 0.01%, rgba(93, 108, 187, 0.41) 59.82%, rgba(0, 19, 115, 0.3) 100%)",
+          "linear-gradient(180deg, rgba(11, 46, 226, 0.3) 100%, rgba(53, 110, 152, 0.25) 0%)",
       },
       borderRadius: {
         main: "10px",
